2a3fd4731e3325f20c3825804a43d33f1f08e186
QuiteChridi <99613353+QuiteChridi@users.noreply.github.com>
2023-11-19 19:24:52 +0100

    Assignment 05: Introduction to the Play Framework completed

---
+++ public/javascripts/ProfilePage.js
@@ -0,0 +1,56 @@
+    // Elemente aus dem DOM auswählen
+    const profileDisplay = document.querySelector(".profile-display");
+    const profileEditor = document.querySelector(".profile-editor");
+    const userNameDisplay = document.getElementById("user-name");
+    const editButton = document.getElementById("edit-button");
+    const nameInput = document.getElementById("name-input");
+    const saveButton = document.getElementById("save-button");
+    const fileInput = document.getElementById("file-input");
+    const profilePictureEdit = document.getElementById("profile-picture-edit");
+    const profilePictureDisplay = document.getElementById("profile-picture");
+
+    // Ursprünglicher Name auf der Anzeigeseite
+    let originalName = userNameDisplay.textContent;
+
+    // Ursprüngliche Bild-URL auf der Anzeigeseite
+    let originalImageUrl = profilePictureDisplay.src;
+
+    // Event-Handler für den "Edit Picture" Button
+    document.getElementById("change-pic-button").addEventListener("click", () => {
+        fileInput.click(); // Öffne den Datei-Explorer, wenn auf den Button geklickt wird
+    });
+
+    // Event-Handler für das Ändern des ausgewählten Bilds
+    fileInput.addEventListener("change", () => {
+        const selectedFile = fileInput.files[0];
+        if (selectedFile) {
+            const imageUrl = URL.createObjectURL(selectedFile);
+            profilePictureEdit.src = imageUrl;
+        }
+    });
+
+    // Event-Handler für den "Edit" Button
+    editButton.addEventListener("click", () => {
+
+        profileDisplay.style.display = "none";
+        profileEditor.style.display = "inline-block"
+
+        // Setze den ursprünglichen Namen als Wert im Eingabefeld
+        nameInput.value = originalName;
+    });
+
+    // Event-Handler für den "Save" Button
+    saveButton.addEventListener("click", () => {
+
+        // Aktualisiere den Benutzernamen mit dem Wert aus dem Eingabefeld
+        const newName = nameInput.value;
+        userNameDisplay.textContent = newName;
+        originalName = newName; // Aktualisiere den ursprünglichen Namen
+
+        // Aktualisiere das Bild auf der Anzeigeseite mit dem ausgewählten Bild oder dem alten Bild
+        profilePictureDisplay.src = profilePictureEdit.src || originalImageUrl;
+        originalImageUrl = profilePictureDisplay.src; // Aktualisiere die ursprüngliche Bild-URL
+
+        profileDisplay.style.display = "inline-block";
+        profileEditor.style.display = "none";
+    });
