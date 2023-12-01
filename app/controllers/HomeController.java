bb5edccbacdd0229c135dd9851916d8e806377a0
QuiteChridi <99613353+QuiteChridi@users.noreply.github.com>
2023-12-01 11:25:40 +0100

    Test

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
 import play.mvc.*;
+import views.html.*;
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -17,16 +22,31 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result main() { return ok(views.html.main.render()); }
-    public Result login() { return ok(views.html.login.render()); }
-    public Result profile() { return ok(views.html.profile.render()); }
-    public Result highscore() {
-        Map<String, Integer> highscores = new HashMap<>();
-        highscores.put("Willi", 0);
-        highscores.put("Hubert", 2);
-        highscores.put("Sepp", 0);
-        highscores.put("Fred", 0);
-        highscores.put("Paul", 0);
 
-        return ok(views.html.highscore.render(highscores)); }
+    public Result main() { return ok(main.render()); }
+    public Result login() {return ok(login.render()); }
+    public Result profile() { return ok(profile.render()); }
+    public Result highscore() {
+        return ok(highscore.render(getHighscores()));
+    }
+
+    private Map<String, Integer> getHighscores(){
+        Map<String, Integer> highscores = new HashMap<>();
+        highscores.put("Willi", (int) (Math.random() * 100));
+        highscores.put("Hubert", (int) (Math.random() * 100));
+        highscores.put("Sepp", (int) (Math.random() * 100));
+        highscores.put("Fred", (int) (Math.random() * 100));
+        highscores.put("Paul", (int) (Math.random() * 100));
+
+        return highscores;
+    }
+
+    public Result authenticate(Http.Request request) {
+        JsonNode json = request.body().asJson();
+        json.findPath("data").textValue();
+
+        ObjectNode result = Json.newObject();
+        result.put("data", "okay");
+        return ok(result);
+    }
 }
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,10 +2,23 @@ function checkLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username === "admin" && password === "admin") {
+    /*if (username === "admin" && password === "admin") {
         // Weiterleitung zur Main Page
         window.location.href = "main";
     } else {
         alert("Falscher Benutzername oder Passwort. Bitte versuchen Sie es erneut.");
-    }
+    }*/
+
+    fetch("/login", {
+        method: "POST",
+        body: JSON.stringify({
+            data: "hello",
+        }),
+        headers: {
+            "Content-Type": "text/json"
+        },
+        credentials: "include"})
+        .then(result => result.json())
+        .then(data => console.log(data));
 }
+
