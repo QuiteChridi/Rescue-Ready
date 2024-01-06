665d7efae64fc205dd922240fadf508bba1ae980
J.Duester <jannis.duester@stud-mail.uni-wuerzburg.de>
2024-01-06 19:05:06 +0100

    Added template tests; Added browser test (not functional); Added Users

---
+++ app/models/User.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.Objects;
+
+public class User {
+    private Integer id;
+    private String name;
+
+    public User(final Integer id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -7,10 +7,13 @@ import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
 
+import play.test.WithBrowser;
+
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
+import static play.test.Helpers.*;
 
 public class HomeControllerTest extends WithApplication {
 
@@ -21,13 +24,46 @@ public class HomeControllerTest extends WithApplication {
 
     @Test
     public void testIndex() {
+        //Simulates HTTP Request to Server
         Http.RequestBuilder request = new Http.RequestBuilder()
                 .method(GET)
                 .header(Http.HeaderNames.HOST, "localhost:19001")
                 .uri("/");
 
+        //Routes simulated Request to Play Application based on URI + Method
         Result result = route(app, request);
         assertEquals(OK, result.status());
     }
 
+    @Test
+    public void testLogin() {
+        Http.RequestBuilder requestBuilder = new Http.RequestBuilder()
+                .method(GET)
+                .header(Http.HeaderNames.HOST, "localhost:19001")
+                .uri("/login");
+        Result result = route(app, requestBuilder);
+        assertEquals(OK, result.status());
+    }
+
+    @Test
+    public void testTemplateContainsJQuery() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .header(Http.HeaderNames.HOST, "localhost:19001")
+                .uri("/");
+        Result result = route(app, request);
+        final String body = contentAsString(result);
+        assertTrue("Template should contain jQuery", body.contains("<script src=\"https://code.jquery.com/jquery-3.6.0.min.js\"></script>"));
+    }
+
+    @Test
+    public void testIndexContainsLoginLink() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .header(Http.HeaderNames.HOST, "localhost:19001")
+                .uri("/");
+        Result result = route(app, request);
+        final String body = contentAsString(result);
+        assertTrue("Index should contain a Login link", body.contains("value=\"Login\""));
+    }
 }
