101616e9e23518df554da82f1f6d6fb5052a823a
QuiteChridi <99613353+QuiteChridi@users.noreply.github.com>
2024-02-10 13:40:08 +0100

    Refactored Profile Pic change

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -4,18 +4,15 @@ function editProfile(){
     document.getElementById("editProfile-container").style.display = "flex"
 }
 
-function saveChanges(){
-    document.getElementById("profile-container").style.display = "flex";
-    document.getElementById("editProfile-container").style.display = "none"
-    saveChangesToUserData();
-    saveProfilePic();
-}
-
-function saveChangesToUserData() {
+function saveChanges() {
     let username = document.getElementById("name-input").value;
     let password = document.getElementById("password-input").value;
     let email = document.getElementById("email-input").value;
-    let profilePicPath = document.getElementById("newProfilePicture").files[0].name;
+    let profilePic = document.getElementById("newProfilePicture").files[0];
+    let profilePicPath = ""
+    if(profilePic != null){
+        profilePicPath = profilePic.name;
+    }
 
     fetch("/saveUser", {
         method: "POST",
@@ -37,29 +34,8 @@ function saveChangesToUserData() {
     }).then(data => {
         if(data.success !== true){
             alert("something went wrong")
+        } else {
+            location.reload();
         }
     }).catch(error => console.log(error.message))
-}
-
-function saveProfilePic() {
-    let profilePicInput = document.getElementById("newProfilePicture");
-    let profilePicFile = profilePicInput.files[0]; // Das ausgewählte Bild
-
-    let formData = new FormData();
-    formData.append("picture", profilePicFile); // Bild zur FormData hinzufügen
-
-    fetch("/saveProfilePic", {
-        method: "POST",
-        body: formData,
-        credentials: "include"
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('HTTP error! Status: ${response.status}');
-            }
-            return response.text();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
 }
